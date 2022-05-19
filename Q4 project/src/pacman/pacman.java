eb948c63b4862f75cf06a6bf8ef8ab0cc3718525
1861626 <1861626@01-19780.pusd.dom>
2022-05-19 11:18:29 -0700

    second commit

--- Q4 project/src/pacman/pacman.java
+++ Q4 project/src/pacman/pacman.java
@@ -102,6 +102,7 @@ public class pacman extends JPanel implements ActionListener {
 
             movePacman();
             drawPacman(g2d);
+            moveGhosts(g2d);
             checkMaze();
         }
     }
@@ -112,7 +113,18 @@ public class pacman extends JPanel implements ActionListener {
         g2d.setColor(Color.yellow);
         g2d.drawString(start, ((SCREEN_SIZE)/4)+17, 170);
     }
+    
+    private void drawScore(Graphics2D g) {
+    g.setFont(smallFont);
+    g.setColor(new Color(5, 181, 79));
+    String s = "Score: " + score;
+    g.drawString(s, SCREEN_SIZE / 2 + 96, SCREEN_SIZE + 16);
 
+    for (int i = 0; i < lives; i++) {
+        g.drawImage(heart, i * 28 + 8, SCREEN_SIZE + 1, this);
+    }
+} 
+    
     private void checkMaze() {
 
         int i = 0;
@@ -153,7 +165,77 @@ public class pacman extends JPanel implements ActionListener {
 
         continueLevel();
     }
+    private void moveGhosts(Graphics2D g2d) {
 
+    int pos;
+    int count;
+
+    for (int i = 0; i < N_GHOSTS; i++) {
+        if (ghost_x[i] % BLOCK_SIZE == 0 && ghost_y[i] % BLOCK_SIZE == 0) {
+            pos = ghost_x[i] / BLOCK_SIZE + N_BLOCKS * (int) (ghost_y[i] / BLOCK_SIZE);
+
+            count = 0;
+
+            if ((screenData[pos] & 1) == 0 && ghost_dx[i] != 1) {
+                dx[count] = -1;
+                dy[count] = 0;
+                count++;
+            }
+
+            if ((screenData[pos] & 2) == 0 && ghost_dy[i] != 1) {
+                dx[count] = 0;
+                dy[count] = -1;
+                count++;
+            }
+
+            if ((screenData[pos] & 4) == 0 && ghost_dx[i] != -1) {
+                dx[count] = 1;
+                dy[count] = 0;
+                count++;
+            }
+
+            if ((screenData[pos] & 8) == 0 && ghost_dy[i] != -1) {
+                dx[count] = 0;
+                dy[count] = 1;
+                count++;
+            }
+
+            if (count == 0) {
+
+                if ((screenData[pos] & 15) == 15) {
+                    ghost_dx[i] = 0;
+                    ghost_dy[i] = 0;
+                } else {
+                    ghost_dx[i] = -ghost_dx[i];
+                    ghost_dy[i] = -ghost_dy[i];
+                }
+
+            } else {
+
+                count = (int) (Math.random() * count);
+
+                if (count > 3) {
+                    count = 3;
+                }
+
+                ghost_dx[i] = dx[count];
+                ghost_dy[i] = dy[count];
+            }
+
+        }
+
+        ghost_x[i] = ghost_x[i] + (ghost_dx[i] * ghostSpeed[i]);
+        ghost_y[i] = ghost_y[i] + (ghost_dy[i] * ghostSpeed[i]);
+        drawGhost(g2d, ghost_x[i] + 1, ghost_y[i] + 1);
+
+        if (pacman_x > (ghost_x[i] - 12) && pacman_x < (ghost_x[i] + 12)
+                && pacman_y > (ghost_y[i] - 12) && pacman_y < (ghost_y[i] + 12)
+                && inGame) {
+
+            dying = true;
+        }
+    }
+} 
     private void drawGhost(Graphics2D g2d, int x, int y) {
     	g2d.drawImage(ghost, x, y, this);
         }
@@ -310,6 +392,7 @@ public class pacman extends JPanel implements ActionListener {
         g2d.fillRect(0, 0, d.width, d.height);
 
         drawMaze(g2d);
+        drawScore(g2d);
 
         if (inGame) {
             playGame(g2d);
